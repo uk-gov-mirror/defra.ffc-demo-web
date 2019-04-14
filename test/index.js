19be3806bad7401b9a3d0ef4f1c2d62b54ca31fc
John Watson <johnwatson484@hotmail.com>
2019-04-14 15:06:32 +0100

    Unit tests updated

--- test/index.js
+++ test/index.js
@@ -6,9 +6,14 @@ const createServer = require('../server')
 lab.experiment('Web test', () => {
   let server
 
-  // Create server before the tests
   lab.before(async () => {
     server = await createServer()
+    await server.initialize()
+  })
+
+  // Stop server after the tests.
+  lab.after(async () => {
+    await server.stop()
   })
 
   lab.test('GET / route works', async () => {
@@ -26,6 +31,61 @@ lab.experiment('Web test', () => {
     const options = {
       method: 'GET',
       url: '/claim/property-type'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+
+  lab.test('GET /accessible route works', async () => {
+    const options = {
+      method: 'GET',
+      url: '/claim/accessible'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+
+  lab.test('GET /date-of-subsidence route works', async () => {
+    const options = {
+      method: 'GET',
+      url: '/claim/date-of-subsidence'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+
+  lab.test('GET /confirmation route works', async () => {
+    const options = {
+      method: 'GET',
+      url: '/claim/confirmation'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+
+  lab.test('GET /email route works', async () => {
+    const options = {
+      method: 'GET',
+      url: '/claim/email'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+
+  lab.test('GET /mine-type route works', async () => {
+    const options = {
+      method: 'GET',
+      url: '/claim/mine-type'
     }
 
     const response = await server.inject(options)
