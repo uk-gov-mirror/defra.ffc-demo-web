a33ac5c1416a6a36e7242739d26458dc1b03b7bb
Fareed Mohammed <fareed.mohammed.uk@gmail.com>
2021-02-08 17:04:04 +0000

    Date validation to only allow past dates (#230)
    * initial:PR creation
    https://eaflood.atlassian.net/browse/PSD-1127
    
    * feat: added validation
    
    * feat: added acceptance tests for date of subsidence validation

--- app/routes/claim/date-of-subsidence.js
+++ app/routes/claim/date-of-subsidence.js
@@ -16,6 +16,14 @@ module.exports = [
           const date = dateOfSubsidence ? new Date(dateOfSubsidence) : undefined
           return { dateOfSubsidence: date }
         },
+        setData: async (request, data) => {
+          const dateOfSubsidence = data.dateOfSubsidence
+          if (dateOfSubsidence > new Date()) {
+            return {
+              errors: { titleText: 'Fix the following errors', errorList: [{ href: '#dateOfSubsidence', name: 'dateOfSubsidence', text: 'Date has to be in the past' }] }
+            }
+          }
+        },
         getNextPath: () => './mine-type',
         pageDefinition: require('./page-definitions/date-of-subsidence')
       }
--- test/integration/narrow/routes/claim/date-of-subsidence.test.js
+++ test/integration/narrow/routes/claim/date-of-subsidence.test.js
@@ -21,6 +21,30 @@ describe('Date of Subsidence test', () => {
     expect(response.statusCode).toBe(200)
   })
 
+  test('GET /claim/date-of-subsidence returns as error if date subsidence is in the future', async () => {
+    const options = {
+      method: 'POST',
+      url: '/claim/date-of-subsidence',
+      payload: { dateOfSubsidence__year: '2022', dateOfSubsidence__month: '01', dateOfSubsidence__day: '01' }
+    }
+
+    const postResponse = await server.inject(options)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('Date has to be in the past')
+  })
+
+  test('GET /claim/date-of-subsidence redirects to next page, if date subsidence is in the past', async () => {
+    const options = {
+      method: 'POST',
+      url: '/claim/date-of-subsidence',
+      payload: { dateOfSubsidence__year: '2021', dateOfSubsidence__month: '01', dateOfSubsidence__day: '01' }
+    }
+
+    const postResponse = await server.inject(options)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('./mine-type')
+  })
+
   afterEach(async () => {
     jest.clearAllMocks()
     await server.stop()
